09b9bf9135653a6441f331ff13d80aa9d16e67ef
Marcian Szlovak <marcianszlovak@gmail.com>
2020-03-17 22:10:39 +0100

    update movie exercise

--- mysql/workshop/movies.sql
+++ mysql/workshop/movies.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Movies
+create TABLE Movies
 (
     id          INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     movie_title VARCHAR(100),
@@ -6,8 +6,18 @@ CREATE TABLE Movies
     year        DATETIME
 );
 
-INSERT INTO Movies (movie_title, director, year)
-VALUES ("Godfather 2", "Francis Ford Coppola", "1974-01-01"),
+insert into Movies (movie_title, director, year)
+values ("Godfather 2", "Francis Ford Coppola", "1974-01-01"),
        ("Scarface", "Brian De Palma", "1983-12-01"),
        ("Goodfellas", "Martin Scorsese", "1991-02-14"),
        ("The Departed", "Martin Scorsese", "2006-09-26");
+
+select Movies.movie_title from Movies join todo_app on Movies.id = todo_app.todo_list;
+
+create TABLE WatchedMovies
+(
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    watched_list INTEGER,
+    FOREIGN KEY (watched_list) REFERENCES Movies (id)
+
+);

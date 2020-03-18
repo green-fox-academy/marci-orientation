89418d82fe8cbf9a0874915147879d2b06bd0fc3
Marcian Szlovak <marcianszlovak@gmail.com>
2020-03-18 21:04:05 +0100

    end of workshop

--- mysql/workshop/movies.sql
+++ mysql/workshop/movies.sql
@@ -1,34 +1,44 @@
 CREATE TABLE Movies
 (
-    id          INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id          TINYINT(1) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     movie_title VARCHAR(100),
     director    VARCHAR(100),
     year        DATETIME
 );
 
-INSERT INTO Movies (movie_title, director, year)
-VALUES ("Godfather 2", "Francis Ford Coppola", "1974-01-01"),
-       ("Scarface", "Brian De Palma", "1983-12-01"),
-       ("Goodfellas", "Martin Scorsese", "1991-02-14"),
-       ("The Departed", "Martin Scorsese", "2006-09-26");
+INSERT INTO Movies
+VALUES (null, "Godfather 2", "Francis Ford Coppola", "1974-01-01"),
+       (null, "Scarface", "Brian De Palma", "1983-12-01"),
+       (null, "Goodfellas", "Martin Scorsese", "1991-02-14"),
+       (null, "The Departed", "Martin Scorsese", "2006-09-26");
 
-SELECT Movies.movie_title FROM Movies JOIN todo_app ON Movies.id = todo_app.todo_list;
+SELECT todo_list
+FROM TodoApp
+         INNER JOIN Movies ON TodoApp.id = Movies.id
+WHERE TodoApp.id = Movies.id;
 
-CREATE TABLE WatchedMovies
+CREATE TABLE MoviesTwo
 (
-    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    watched_list INTEGER,
-    FOREIGN KEY (watched_list) REFERENCES Movies (id)
-
+    watched_id  TINYINT NOT NULL,
+    to_watch_id TINYINT NOT NULL,
+    movie_list  VARCHAR(50),
+    FOREIGN KEY (watched_id) REFERENCES TodoApp (id),
+    FOREIGN KEY (to_watch_id) REFERENCES Movies (id)
 );
 
-CREATE TABLE Movies
-(
-    id           INTEGER PRIMARY KEY AUTO_INCREMENT,
-    movie_title  VARCHAR(100),
-    director     VARCHAR(100),
-    year         DATETIME,
-    watched_list VARCHAR(50) NOT NULL ,
-    FOREIGN KEY (watched_list) REFERENCES todo_app (todo_list)
+ALTER TABLE MoviesTwo
+    RENAME COLUMN watched_id TO Todo_id;
+ALTER TABLE MoviesTwo
+    RENAME COLUMN to_watch_id TO Movies_id;
+ALTER TABLE MoviesTwo
+    RENAME COLUMN movie_list TO Movie_list;
 
-);
+INSERT INTO MoviesTwo
+VALUES (1, 1, "Gladiator");
+
+INSERT INTO MoviesTwo (Todo_id, Movies_id, Movie_list)
+VALUES (null, null, null)
+
+--    will get the error: [2020-03-18 20:34:42] [23000][1048] Column 'Todo_id' cannot be null
+
+DROP TABLE Movies, MoviesTwo, TodoApp;
--- mysql/workshop/todo.sql
+++ mysql/workshop/todo.sql
@@ -5,8 +5,9 @@ CREATE TABLE todo_app
     done_list VARCHAR(100) NOT NULL
 );
 
-INSERT INTO todo_app (todo_list, done_list)
-VALUES ("play games", "do laundry"),
-       ("listen to matreview", "get up from bed");
+INSERT INTO todo_app
+VALUES (null, "play games", "do laundry"),
+       (null, "listen to matreview", "get up from bed");
 
 rename TABLE todo_app TO TodoApp;
+

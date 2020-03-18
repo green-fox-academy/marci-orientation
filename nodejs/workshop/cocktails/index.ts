4d81fd66a801bf8f6fdc93f48f5e2e620401e6f9
Marcian Szlovak <marcianszlovak@gmail.com>
2020-03-18 01:47:34 +0100

    intellij settings

--- nodejs/workshop/cocktails/index.ts
+++ nodejs/workshop/cocktails/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import { cocktails } from "./assets/cocktails";
-import { alcoholList } from "./assets/alcohol-list";
+import {cocktails} from "./assets/cocktails";
+import {alcoholList} from "./assets/alcohol-list";
 
 const app = express();
 const PORT = 3000;
@@ -8,21 +8,23 @@ const viewEngine = app.set("view engine", "ejs");
 const expressStatic = app.use("/static", express.static("static"));
 
 app.get("/", (req: any, res: any) => {
-  if (req.query.alcohol) {
-    const filteredCocktails = cocktails.filter(cocktail =>
-      cocktail.contains.includes(req.query.alcohol)
-    );
-    res.render("main", {
-      cocktails: filteredCocktails,
-      alcoholList: alcoholList
-    });
-  } else {
-    res.render("main", { cocktails: cocktails, alcoholList: alcoholList });
-  }
+    if (req.query.alcohol) {
+        const filteredCocktails = cocktails.filter(cocktail =>
+            cocktail.contains.includes(req.query.alcohol)
+        );
+        res.render("main", {
+            cocktails: filteredCocktails,
+            alcoholList: alcoholList
+        });
+    } else {
+        res.render("main", {cocktails: cocktails, alcoholList: alcoholList});
+    }
 });
 
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+
+
+    console.log(`Listening on port ${PORT}`);
 });
 
 // const inner: boolean[] = cocktails.map(e => {

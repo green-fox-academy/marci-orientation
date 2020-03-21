1da1541c8c2a786783ac4952e8099384a26f4921
Marcian Szlovak <marcianszlovak@gmail.com>
2020-03-21 18:52:21 +0100

    qol fixes in bomb-defuse exercise

--- callback/bomb-defuse/main.js
+++ callback/bomb-defuse/main.js
@@ -3,35 +3,36 @@
 const bomb = document.getElementsByClassName("display")[0];
 const button = document.getElementById("defused");
 const warning = document.getElementById("run");
-const detonationTime = 0;
+const detonationTime = -1;
 const warningTime = 2;
 let counter = 10;
 
 const temp = setInterval(() => {
-  if (counter === warningTime) {
-    warning.style.display = "flex";
+  if (counter <= warningTime) {
+    warning.style.display = "block";
   }
 
   if (counter === detonationTime) {
     warning.style.display = "none";
-    bomb.innerHTML = `Terrorists win.`;
+    bomb.textContent = `Terrorists win.`;
   } else {
-    bomb.innerHTML = counter--;
+    bomb.textContent = counter--;
   }
 }, 1000);
 
-const defused = (button.onclick = () => {
-  if (counter !== detonationTime) {
+
+button.onclick = () => {
+  if (counter > detonationTime) {
     clearInterval(temp);
 
-    button.innerHTML = `Bomb has been defused. Counter-Terrorists win.`;
+    button.textContent = `Bomb has been defused. Counter-Terrorists win.`;
   }
 
   if (counter === detonationTime) {
-    button.innerHTML = `The bomb has exploded, better luck next time`;
+    button.textContent = `The bomb has exploded, better luck next time`;
   }
 
   if (counter <= warningTime) {
     warning.style.display = "none";
   }
-});
+};

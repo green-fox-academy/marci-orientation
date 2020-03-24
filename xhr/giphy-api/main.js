61cb3e45fca102adfaf453c6565aa86ed71429ec
marci <marcianszlovak@gmail.com>
2020-03-24 22:15:41 +0100

    giphy api exercise almost done, still need onclick event

---
+++ xhr/giphy-api/main.js
@@ -0,0 +1,39 @@
+"use strict";
+var GIPHY = /** @class */ (function () {
+    function GIPHY(API_URL, API_KEY, myRequest, myGIF, asd) {
+        if (API_URL === void 0) { API_URL = "https://api.giphy.com/v1/gifs/search"; }
+        if (API_KEY === void 0) { API_KEY = "vtAtAQfLZXRy6YbSA8EPnPzwfwtRvbcQ"; }
+        if (myRequest === void 0) { myRequest = new XMLHttpRequest(); }
+        if (myGIF === void 0) { myGIF = document.getElementById("gif-container"); }
+        if (asd === void 0) { asd = document.createElement("img"); }
+        this.API_URL = API_URL;
+        this.API_KEY = API_KEY;
+        this.myRequest = myRequest;
+        this.myGIF = myGIF;
+        this.asd = asd;
+    }
+    GIPHY.prototype.load = function () {
+        var _this = this;
+        this.myRequest.onload = function () {
+            if (_this.myRequest.status === 200) {
+                var myResponse = JSON.parse(_this.myRequest.responseText);
+                myResponse.data.forEach(function (e) {
+                    var GIF = document.createElement("img");
+                    GIF.setAttribute("src", e.images.fixed_width_small_still.url);
+                    _this.myGIF.appendChild(GIF);
+                });
+            }
+        };
+    };
+    GIPHY.prototype.open = function () {
+        this.myRequest.open("GET", this.API_URL + "?api_key=" + this.API_KEY + "&q=random&limit=16");
+    };
+    GIPHY.prototype.send = function () {
+        this.myRequest.send();
+    };
+    return GIPHY;
+}());
+var GIFGenerator = new GIPHY();
+GIFGenerator.load();
+GIFGenerator.open();
+GIFGenerator.send();
---
+++ xhr/giphy-api/main.ts
@@ -0,0 +1,51 @@
+"use strict";
+
+class GIPHY {
+  public API_URL: string;
+  private readonly API_KEY: string;
+  private myRequest: XMLHttpRequest;
+  private myGIF: HTMLElement;
+    public asd: HTMLImageElement;
+  constructor(
+    API_URL: string = "https://api.giphy.com/v1/gifs/search",
+    API_KEY: string = "vtAtAQfLZXRy6YbSA8EPnPzwfwtRvbcQ",
+    myRequest: XMLHttpRequest = new XMLHttpRequest(),
+    myGIF: HTMLElement = document.getElementById("gif-container"), asd: HTMLImageElement = document.createElement("img")
+  ) {
+    this.API_URL = API_URL;
+    this.API_KEY = API_KEY;
+    this.myRequest = myRequest;
+    this.myGIF = myGIF;
+      this.asd = asd;
+  }
+
+  public load(): void {
+    this.myRequest.onload = () => {
+      if (this.myRequest.status === 200) {
+        const myResponse: any = JSON.parse(this.myRequest.responseText);
+
+        myResponse.data.forEach((e) => {
+          const GIF: HTMLImageElement = document.createElement("img");
+          GIF.setAttribute("src", e.images.fixed_width_small_still.url);
+          this.myGIF.appendChild(GIF);
+        });
+      }
+    };
+  }
+
+  public open(): void {
+    this.myRequest.open(
+      "GET",
+      `${this.API_URL}?api_key=${this.API_KEY}&q=random&limit=16`
+    );
+  }
+
+  public send(): void {
+    this.myRequest.send();
+  }
+}
+
+const GIFGenerator: GIPHY = new GIPHY();
+GIFGenerator.load();
+GIFGenerator.open();
+GIFGenerator.send();

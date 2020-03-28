820b38a521efcf6b112e4e32b96457f8c70a08fb
marci <marcianszlovak@gmail.com>
2020-03-28 14:30:35 +0100

    appenda exercise finished

--- rest-api-backend/app.ts
+++ rest-api-backend/app.ts
@@ -1,19 +1,51 @@
 import * as express from "express";
 import * as path from "path";
 import * as bodyParser from "body-parser";
-import { response } from "express";
 
-export default class App {
+class App {
   public app: express.Application;
   public port: number;
   private parser: express.Application;
-  public container;
 
   constructor() {
     this.app = express();
     this.port = 8080;
     this.parser = this.app.use(bodyParser.json());
-    this.container = {};
+  }
+
+  public double(): void {
+    this.app.get(
+      "/doubling",
+      (request: express.Request, response: express.Response) => {
+        let myNum: number = request.query.input;
+        let container: { [key: string]: any } = {};
+
+        if (myNum) {
+          container = {
+            received: myNum,
+            result: myNum * 2,
+          };
+        } else {
+          container = {
+            error: "Please provide an input!",
+          };
+        }
+        response.send(container);
+      }
+    );
+  }
+
+  public appendA(): void {
+    this.app.get(
+      "/appenda/:input",
+      (request: express.Request, response: express.Response) => {
+        let input: string = request.params.input;
+        let container: { [key: string]: any } = {};
+
+        container = { appended: `${input}a` };
+        response.send(container);
+      }
+    );
   }
 
   public assets(): void {
@@ -24,24 +56,25 @@ export default class App {
     this.app.get(
       "/greeter",
       (request: express.Request, response: express.Response) => {
-        let name = request.query.name;
-        let title = request.query.title;
+        let name: string = request.query.name;
+        let title: string = request.query.title;
+        let container: { [key: string]: any } = {};
 
         if (name && title) {
-          this.container = {
+          container = {
             welcome_message: `Oh, hi there ${name}, my dear ${title}!`,
           };
         } else if (!name && !title) {
-          this.container = { error: "Please provide a name and a title!" };
+          container = { error: "Please provide a name and a title!" };
           response.status(400);
         } else if (!name) {
-          this.container = { error: "Please provide a name!" };
+          container = { error: "Please provide a name!" };
           response.status(400);
         } else if (!title) {
-          this.container = { error: "Please provide a title!" };
+          container = { error: "Please provide a title!" };
           response.status(400);
         }
-        response.send(this.container);
+        response.send(container);
       }
     );
   }
@@ -51,21 +84,6 @@ export default class App {
       "/",
       (request: express.Request, response: express.Response) => {
         response.sendFile(path.join(__dirname, "index.html"));
-      }
-    );
-  }
-
-  public double(): void {
-    this.app.get(
-      "/doubling",
-      (request: express.Request, response: express.Response) => {
-        let myNum = request.query.input;
-
-        if (myNum) {
-          response.json({ received: Number(myNum), result: myNum * 2 });
-        } else {
-          response.json({ error: "Please provide an input!" });
-        }
       }
     );
   }
@@ -83,3 +101,6 @@ temp.assets();
 temp.listen();
 temp.double();
 temp.greet();
+temp.appendA();
+
+
---
+++ rest-api-backend/router.ts
@@ -0,0 +1,10 @@
+import * as express from "express";
+
+
+const router = express.Router;
+
+router.get("/", (req, res) => {
+  res.send("hello");
+});
+
+module.exports = router;

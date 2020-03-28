8e6e582269936fe1b7f306cfdc1f826b5df4cc07
marci <marcianszlovak@gmail.com>
2020-03-28 18:59:58 +0100

    add .js files to gitignore

--- rest-api-backend/src/Server.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var App_1 = require("./App");
-var Router_1 = require("./Router");
-var app = new App_1.default([new Router_1.default()], 8080);
-app.listen();

9b739a193eb152477385471f6288b97981de0adf
marci <marcianszlovak@gmail.com>
2020-03-31 21:34:22 +0200

    rewrite sql-server.ts

--- nodejs-mysql/sql-server.ts
+++ nodejs-mysql/sql-server.ts
@@ -2,17 +2,17 @@ import * as mariadb from "mariadb";
 
 const pool = mariadb.createPool({
   host: "localhost",
-  user: "root",
-  password: "vlagyimirzon",
-  database: "BookStore",
-  connectionLimit: 5,
+  user: "marci-greenfox",
+  password: "password",
+  database: "mydb",
+  connectionLimit: 1,
 });
 
 export async function asyncFunction() {
   let conn;
   try {
     conn = await pool.getConnection();
-    const res = await conn.query("SELECT aut_id FROM author;");
+    const res = await conn.query("SELECT aut_id FROM mydb.author");
     console.log(res);
     // const rows = await conn.query("")
   } catch (err) {

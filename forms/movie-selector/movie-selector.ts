ee7ab4ee44d299adca21cd05768817704233db05
marci <marcianszlovak@gmail.com>
2020-04-18 19:18:25 +0200

    movie-selector halfway done

---
+++ forms/movie-selector/movie-selector.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+const movies: { [key: string]: any } = [
+  { movie: 'Moon', genre: 'sci-fi' },
+  { movie: '2001: A Space Odyssey', genre: 'sci-fi' },
+  { movie: 'Contact', genre: 'drama' },
+  { movie: 'Darkest Hour', genre: 'drama' },
+  { movie: 'There Will Be Blood', genre: 'drama' },
+  { movie: 'American Beauty', genre: 'comedy' },
+  { movie: 'Airplane!', genre: 'comedy' },
+  { movie: 'Deadpool', genre: 'comedy' },
+  { movie: "Wayne's World", genre: 'comedy' },
+];

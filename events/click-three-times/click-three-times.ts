0de44f7e93cfc8bb829f47c0541e7e296e2a0007
marci <marcianszlovak@gmail.com>
2020-04-19 00:47:54 +0200

    rewrite click three times exercise

--- events/click-three-times/click-three-times.ts
+++ events/click-three-times/click-three-times.ts
@@ -1,14 +1,31 @@
-import { fromEvent, Observable } from "rxjs";
+import {
+  fromEvent,
+  Observable,
+  Observer,
+  Subscriber,
+  Subscription,
+} from 'rxjs';
+import { take } from 'rxjs/operators';
 
-const button: HTMLButtonElement = document.querySelector("button");
-const message = document.querySelector(".result");
+const button: HTMLButtonElement = document.querySelector('button');
+const message: Element = document.querySelector('.result');
+const click$: Observable<Event> = fromEvent(button, 'click').pipe(take(3));
 
-const click: Observable<Event> = fromEvent(button, "click");
+const observer: Observer<Event> = {
+  next: (value) => console.log(value),
+  error: (error) => console.log('error', error),
+  complete: () => (message.innerHTML = '5 seconds passed and clicked 3 times'),
+};
 
-click.subscribe((event: Event) => {
-  let counter: number = 0;
-  counter++;
-  if (event.timeStamp > 5000 && counter === 3) {
-    message.innerHTML = "5 seconds elapsed and clicked 3 times";
+const observable: Observable<unknown> = new Observable(
+  (subscriber: Subscriber<string>) => {
+    subscriber.next('logging your clicks, brah');
   }
-});
+);
+
+const subscription: Subscription = observable.subscribe(observer);
+
+setTimeout(() => {
+  subscription.unsubscribe();
+  click$.subscribe(observer);
+}, 5000);

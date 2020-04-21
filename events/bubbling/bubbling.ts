cf6383658fb54cea968b04c523b6fb533be6e32c
marci <marcianszlovak@gmail.com>
2020-04-21 03:08:02 +0200

    add even shorter solution with mergeMap

--- events/bubbling/bubbling.ts
+++ events/bubbling/bubbling.ts
@@ -1,9 +1,9 @@
-import { fromEvent } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { fromEvent, Observable } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
 
 const nav: HTMLElement = document.querySelector('nav');
 const imgInspector: Element = document.querySelector('.img-inspector');
-const click$ = fromEvent(nav, 'click');
+const click$: Observable<Event> = fromEvent(nav, 'click');
 
 const initialState = {
   size: 200,
@@ -18,25 +18,18 @@ const setAttribute = (a: number, b: number, c: number) => {
   );
 };
 
+//prettier-ignore
+
 click$
   .pipe(
-    map(() => {
-      event.target.innerText === 'Up' ? setAttribute(0, -10, 0) : null;
-    }),
-    map(() => {
-      event.target.innerText === 'Down' ? setAttribute(0, 10, 0) : null;
-    }),
-    map(() => {
-      event.target.innerText === 'Left' ? setAttribute(0, 0, -10) : null;
-    }),
-    map(() => {
-      event.target.innerText === 'Right' ? setAttribute(0, 0, 10) : null;
-    }),
-    map(() => {
-      event.target.innerText === 'Zoom in' ? setAttribute(20, 0, 0) : null;
-    }),
-    map(() => {
-      event.target.innerText === 'Zoom out' ? setAttribute(-20, 0, 0) : null;
-    })
-  )
-  .subscribe();
+    mergeMap(() => {
+      return {
+        up: event.target.innerText === 'Up' ? setAttribute(0, -10, 0) : null,
+        down: event.target.innerText === 'Down' ? setAttribute(0, 10, 0) : null,
+        left: event.target.innerText === 'Left' ? setAttribute(0, 0, -10) : null,
+        right: event.target.innerText === 'Right' ? setAttribute(0, 0, 10) : null,
+        zoomIn: event.target.innerText === 'Zoom in' ? setAttribute(20, 0, 0) : null,
+        zoomOut: event.target.innerText === 'Zoom out' ? setAttribute(-20, 0, 0) : null,
+    };
+}))
+    .subscribe();

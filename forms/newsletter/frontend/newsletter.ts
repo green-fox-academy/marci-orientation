c6c5ed955696cdabe0483009a3bb5b2caed69852
marci <marcianszlovak@gmail.com>
2020-04-24 21:55:48 +0200

    rewrite newsletter frontend

--- forms/newsletter/frontend/newsletter.ts
+++ forms/newsletter/frontend/newsletter.ts
@@ -1,32 +1,26 @@
 import { Observable, of, fromEvent } from 'rxjs';
-import { ajax, AjaxRequest, AjaxResponse } from 'rxjs/ajax';
+import { AjaxResponse } from 'rxjs/ajax';
 import { map, catchError, switchMap } from 'rxjs/operators';
+import { ajaxPost } from 'rxjs/internal/observable/dom/AjaxObservable';
 
 const form: HTMLFormElement = document.querySelector('form');
 const eMail: HTMLInputElement = document.getElementById('email');
 const userName: HTMLInputElement = document.getElementById('username');
 
-const users: Observable<AjaxRequest> = ajax({
-  url: 'http://localhost:3000/signup',
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: {
-    username: userName.value,
-    email: eMail.value,
-  },
-}).pipe(
-  map((response: AjaxResponse) => console.log('response: ', response)),
+const submitted: Observable<Event> = fromEvent(form, 'submit').pipe(
+  map((event) => event.preventDefault()),
+  switchMap(() =>
+    ajaxPost(
+      'http://localhost:3000/signup',
+      { username: userName.value, email: eMail.value },
+      { 'Content-Type': 'application/json' }
+    )
+  ),
+  map((response: AjaxResponse) => console.log('response ', response)),
   catchError((error: any) => {
     console.log('error: ', error);
     return of(error);
   })
 );
 
-const submitForm: Observable<AjaxRequest> = fromEvent(form, 'submit').pipe(
-  map((event: Event) => event.preventDefault()),
-  switchMap(() => users)
-);
-
-submitForm.subscribe();
+submitted.subscribe();

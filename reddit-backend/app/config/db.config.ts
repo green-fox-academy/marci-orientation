29fff9375304043a0cbb09b7122a1c6fb96d6d8c
marci <marcianszlovak@gmail.com>
2020-04-26 02:13:22 +0200

    delete old reddit-backend folder

--- reddit-backend/app/config/db.config.ts
+++
@@ -1,6 +0,0 @@
-export const dbConfig = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "password",
-  DB: "reddit",
-};
--- reddit-backend/app/routes/post.routes.ts
+++
@@ -1,8 +0,0 @@
-// Get all Posts
-
-module.exports = (app: any) => {
-  const posts = require("../controller/post.controller");
-  app.get("/posts", posts.getAllPosts);
-
-  app.post("/posts", posts.create);
-};

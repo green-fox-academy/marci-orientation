b3342d60a2dd250be29fdd7b19063e5ac8a17792
marci <marcianszlovak@gmail.com>
2020-04-27 22:59:59 +0200

    add dist folder to gitignore

--- reddit/backend/dist/posts/dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class CreatePostDto {
-    readonly id: number;
-    readonly title: string;
-    readonly url: string;
-    readonly timestamp: number;
-    readonly score: number;
-    readonly owner: string;
-    readonly vote: number;
-}
--- reddit/backend/dist/posts/post.entity.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class rPost {
-    id: number;
-    title: string;
-    url: string;
-    timestamp: number;
-    score: number;
-    owner: string;
-    vote: number;
-}

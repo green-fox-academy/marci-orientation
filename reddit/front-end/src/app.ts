917e7cf211a7a08041d4d00e79d4023b0c47bedd
marci <marcianszlovak@gmail.com>
2020-05-03 17:52:31 +0200

    update css, add missing annotations, add pseudocode

--- reddit/front-end/src/app.ts
+++ reddit/front-end/src/app.ts
@@ -1,12 +1,16 @@
-import { fromEvent, of } from 'rxjs';
+import { fromEvent, of, Observable } from 'rxjs';
 import { mergeMap, map, switchMap, catchError, tap } from 'rxjs/operators';
 import { fromFetch } from 'rxjs/fetch';
 import { AjaxResponse } from 'rxjs/ajax';
 import { ajaxPost, ajaxPut } from 'rxjs/internal/observable/dom/AjaxObservable';
 
 const button = document.getElementById('submit-button');
-const upvote: HTMLImageElement = document.getElementById('upvote');
-const downvote: HTMLImageElement = document.getElementById('downvote');
+const upvote: HTMLImageElement = document.getElementById(
+  'upvote'
+) as HTMLImageElement;
+const downvote: HTMLImageElement = document.getElementById(
+  'downvote'
+) as HTMLImageElement;
 const voteCount = document.getElementById('vote-count');
 let clicks = 0;
 
@@ -32,9 +36,27 @@ fromEvent(downvote, 'click')
 //   voteCount.innerHTML = clicks;
 // });
 
+const article = document.getElementById('post-1');
+console.log('hello', article.id);
+const postTitle = article.getElementsByClassName('post-title')[0];
+console.log(postTitle.textContent);
+postTitle.textContent = 'hello';
+
+const newPost = document.createElement('p');
+article.appendChild(newPost);
+
+// Get the last <li> element ("Milk") of <ul> with id="myList2"
+const itm = document.getElementById('post-1');
+
+// Copy the <li> element and its child nodes
+const cln = itm.cloneNode(true);
+
+// Append the cloned <li> element to <ul> with id="myList1"
+document.getElementsByTagName('body')[0].appendChild(cln);
+
 let votes: number = 1;
 
-const submitted$ = fromEvent(button, 'click').pipe(
+const submitted$: Observable<any> = fromEvent(button, 'click').pipe(
   switchMap(() =>
     ajaxPut(
       'http://localhost:3000/posts/2/upvote',
@@ -54,3 +76,12 @@ const submitted$ = fromEvent(button, 'click').pipe(
 );
 
 submitted$.subscribe();
+
+// getAllPostsFromService.subscribe(
+// for posts {
+//   cloneArticle
+//   clonedArticle.id = post.id
+//   clonedArticle.getChild(postTitle) = post.title
+// }
+
+// )

c4dbfe90bb5a21f9133eada4ba1294fc4535077f
marci <marcianszlovak@gmail.com>
2020-05-16 12:44:52 +0200

    add alert for empty fields

--- reddit/front-end/src/app.ts
+++ reddit/front-end/src/app.ts
@@ -1,9 +1,15 @@
 import { fromEvent, of, Observable } from 'rxjs';
-import { map, switchMap, catchError, mergeMap } from 'rxjs/operators';
+import {
+  map,
+  switchMap,
+  catchError,
+  distinctUntilKeyChanged,
+} from 'rxjs/operators';
 import { fromFetch } from 'rxjs/fetch';
 import { AjaxResponse, ajax, AjaxRequest } from 'rxjs/ajax';
 import { ajaxPost } from 'rxjs/internal/observable/dom/AjaxObservable';
 
+const post: HTMLElement = document.getElementById('post');
 const postSection: HTMLElement = document.getElementsByTagName('section')[0];
 
 const submitInfo: HTMLParagraphElement = document.getElementById(
@@ -13,8 +19,12 @@ const postTitle: HTMLHeadElement = document.getElementById('content-header');
 const voteCount: HTMLDivElement = document.getElementById(
   'vote-counter'
 ) as HTMLDivElement;
-
-const post: HTMLElement = document.getElementById('post');
+const upvote: HTMLDivElement = document.getElementById(
+  'upvote'
+) as HTMLDivElement;
+const downvote: HTMLDivElement = document.getElementById(
+  'downvote'
+) as HTMLDivElement;
 
 const submitNewPostForm: HTMLFormElement = document.querySelector('form');
 const submitPostTitle: HTMLInputElement = document.getElementById(
@@ -27,90 +37,6 @@ const submitPostUrl: HTMLInputElement = document.getElementById(
 const deletePostBtn: HTMLCollectionOf<HTMLButtonElement> = document.getElementsByTagName(
   'button'
 );
-
-console.log(submitNewPostForm);
-//creating new post
-const createNewPost$: Observable<AjaxRequest> = ajax({
-  url: 'http://localhost:3000/posts',
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: {
-    title: submitPostTitle.value,
-    url: submitPostUrl.value,
-    owner: 'Donald Trump',
-    timestamp: 2,
-    score: 0,
-    vote: 0,
-  },
-}).pipe(
-  map((response) => console.log('response: ', response)),
-  catchError((error) => {
-    console.log('error: ', error);
-    return of(error);
-  })
-);
-
-fromEvent(submitNewPostForm, 'submit')
-  .pipe(
-    map((event) => event.preventDefault()),
-    switchMap(() =>
-      ajaxPost('http://localhost:3000/posts', {
-        title: submitPostTitle.value,
-        url: submitPostUrl.value,
-        owner: 'Donald Trump',
-        timestamp: 2,
-        score: 0,
-        vote: 0,
-      })
-    ),
-    catchError((error: any) => {
-      console.log('error: ', error);
-      return of(error);
-    })
-  )
-  .subscribe(console.log);
-
-//click on submit button to create a new post
-
-const identifier = deletePostBtn[0];
-console.log('parent nodes', identifier.parentElement.id);
-
-//delete post by article id
-const deletePost$: Observable<AjaxRequest> = ajax({
-  url: `http://localhost:3000/posts/:id/delete`,
-  method: 'DELETE',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: {
-    id: 2,
-  },
-}).pipe(
-  map((response: AjaxResponse) => console.log('response: ', response)),
-  catchError((error: any) => {
-    console.log('error: ', error);
-    return of(error);
-  })
-);
-
-const deletePost = fromEvent(deletePostBtn, 'click').pipe(
-  switchMap(() => deletePost$)
-);
-
-function displayInfo(x: any): void {
-  for (let i = 0; i < x.length; i++) {
-    const article: HTMLElement = post;
-    const clonedPost: Node = post.cloneNode(true);
-    postSection.insertBefore(clonedPost, postSection.childNodes[2]);
-    postTitle.innerHTML = x[i].title + ` ${x[i].id}`;
-    voteCount.innerHTML = x[i].vote;
-    submitInfo.innerText = `Submitted ${x[i].timestamp} years ago by ${x[i].owner}`;
-    article.id = x[i].id;
-    // deletePost.id = `delete-post-${x[i].id}`;
-  }
-}
 
 //fetch all posts
 const data$: Observable<AjaxRequest> = fromFetch(
@@ -128,7 +54,7 @@ const data$: Observable<AjaxRequest> = fromFetch(
     return of({ error: true, message: error.message });
   })
 );
-//use helper function to output data into the DOM and copy event listeners for cloned nodes
+
 data$
   .pipe(
     map((x: AjaxRequest) => displayInfo(x)),
@@ -136,6 +62,67 @@ data$
   )
   .subscribe(console.log);
 
-// data$.subscribe((x: AjaxRequest) => {
-//   displayInfo(x);
-// });
+//creating new post
+const createNewPost$ = fromEvent(submitNewPostForm, 'submit').pipe(
+  map((event) => event.preventDefault()),
+
+  switchMap(() =>
+    ajaxPost('http://localhost:3000/posts', {
+      title: submitPostTitle.value,
+      url: submitPostUrl.value,
+      owner: 'submitInfo',
+      timestamp: 2,
+      score: 0,
+      vote: 0,
+    })
+  ),
+  // distinctUntilKeyChanged('request'),
+  catchError((error: any) => {
+    console.log('error: ', error);
+    alert('Post and URL fields cannot be empty');
+    return of(error);
+  })
+);
+
+createNewPost$.subscribe(console.log);
+
+function displayInfo(x: any): void {
+  for (let i = 0; i < x.length; i++) {
+    const article: HTMLElement = post;
+    const clonedPost: Node = post.cloneNode(true);
+    postSection.insertBefore(clonedPost, postSection.childNodes[2]);
+    postTitle.innerHTML = x[i].title + ` ${x[i].id}`;
+    voteCount.innerHTML = x[i].vote;
+    submitInfo.innerText = `Submitted ${x[i].timestamp} years ago by ${x[i].owner}`;
+    article.id = x[i].id;
+    // deletePost.id = `delete-post-${x[i].id}`;
+  }
+}
+
+fromEvent(upvote, 'click').subscribe(console.log);
+fromEvent(downvote, 'click').subscribe(console.log);
+
+const identifier = deletePostBtn[0];
+console.log('parent nodes', identifier.parentElement.id);
+
+//delete post by article id
+const deletePost$: Observable<AjaxRequest> = ajax({
+  url: `http://localhost:3000/posts/:id/delete`,
+  method: 'DELETE',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: {
+    id: {},
+  },
+}).pipe(
+  map((response: AjaxResponse) => console.log('response: ', response)),
+  catchError((error: any) => {
+    console.log('error: ', error);
+    return of(error);
+  })
+);
+
+const deletePost = fromEvent(deletePostBtn, 'click').pipe(
+  switchMap(() => deletePost$)
+);

0f25d3d68a70853e5a121d216b7ab702c7e24035
marci <marcianszlovak@gmail.com>
2020-05-23 14:19:07 +0200

    add volume control v1

--- music-player/front-end/src/index.ts
+++ music-player/front-end/src/index.ts
@@ -1,17 +1,40 @@
-import { fromEvent, from } from 'rxjs';
-import { map, switchMap, takeUntil, takeWhile } from 'rxjs/operators';
+import { fromEvent, of, from } from 'rxjs';
+import {
+  map,
+  switchMap,
+  takeUntil,
+  takeWhile,
+  tap,
+  pluck,
+  mergeMap,
+} from 'rxjs/operators';
 
 const playButton = document.getElementById('play');
 const song: HTMLAudioElement = document.getElementById(
   'song'
 ) as HTMLAudioElement;
+const volumeControl = of(document.getElementById('volume-control')).pipe(
+  pluck('value')
+);
+const temp = document.getElementById('shuffle');
 
-fromEvent(playButton, 'click').subscribe(() => play());
+song.volume = 0.1;
 
-function play() {
+fromEvent(playButton, 'click')
+  .pipe(
+    mergeMap(() => volumeControl),
+    map(console.log)
+  )
+  .subscribe(() => play());
+
+function play(): void {
   if (song.paused) {
     song.play();
   } else {
     song.pause();
   }
 }
+
+// console.log(volumeControl);
+
+// volumeControl
